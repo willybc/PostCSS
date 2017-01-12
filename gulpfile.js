b2e3cb4cbd3adfba5f3b7ae9b1eea1f944a5ebe1
Willy Bardales <willybc06@gmail.com>
2017-01-12 19:49:43 -0300

    Configurando CSSNEXT en nuestro archivo Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp')
 var postcss = require('gulp-postcss')
-var autoprefixer = require('autoprefixer')
+var cssnext = require('postcss-cssnext')
 var cssnested = require('postcss-nested')
 var browserSync = require('browser-sync').create()
 
@@ -15,10 +15,9 @@ gulp.task('serve', function() {
 
 // Tarea para procesar el CSS
 gulp.task('css', function () {
-
   var processors = [
-    autoprefixer({ browsers: ['> 5%', 'ie 8']}),
-    cssnested
+    cssnested,
+    cssnext({ browsers: ['> 5%', 'ie 8'] })
   ]
 
   return gulp.src('./src/*.css')
