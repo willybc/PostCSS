f1b13a760a3c20a7dc4168750eaefe5b1b646872
Willy Bardales <willybc06@gmail.com>
2017-01-12 19:19:47 -0300

    Configurando nuestras tareas en Gulp: Servidor, Procesamiento de CSS y Watch

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp')
+var postcss = require('gulp-postcss')
+var browserSync = require('browser-sync').create()
+
+
+// Servidor de desarrollo
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    }
+  })
+})
+
+// Tarea para procesar el CSS
+gulp.task('css', function () {
+
+  var processors = []
+
+  return gulp.src('./src/*.css')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream())
+})
+
+// Tarea para vigilar los cambios
+gulp.task('watch', function () {
+  gulp.watch('./src/*.css', ['css'])
+  gulp.watch('../dist/*.html').on('change', browserSync.reload)
+})
+
+gulp.task('default', ['watch', 'serve'])
